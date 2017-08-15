6e93300a6adb654a73ce50bcd11a390a4198a975
Tomas Čerkauskas <tomas@cerkauskas.lt>
2017-08-15 09:05:25 +0200

    update: kNN implementation

--- KNearestNeighbor.py
+++ KNearestNeighbor.py
@@ -1,4 +1,5 @@
 import numpy as np
+import operator
 
 
 class KNearestNeighbor(object):
@@ -14,15 +15,42 @@ class KNearestNeighbor(object):
     def _distance(self, a, b):
         return np.linalg.norm(a-b)
 
+    def _keep(self, items, new_item):
+        if len(items) < self._k:
+            items.append(new_item)
+            return items
+
+        i = 0
+        for item in items:
+            if items[i]['distance'] > new_item['distance']:
+                items.insert(i, new_item)
+                break
+
+            i += 1
+
+        return items[:self._k]
+
+    def _choose_majority(self, items):
+        options = {}
+
+        for item in items:
+            label = item['label']
+            if label not in options:
+                options[label] = 1
+                continue
+
+            options[label] += 1
+
+        return max(options.iteritems(), key=operator.itemgetter(1))[0]
+
     def predict(self, features):
-        minimum_distance = float('Inf')
-        label = None
+        candidates = []
 
         for i in range(len(self._training_data)):
             distance = self._distance(features, self._training_data[i])
+            candidates = self._keep(candidates, {
+                'label': self._training_labels[i],
+                'distance': distance
+            })
 
-            if minimum_distance > distance:
-                minimum_distance = distance
-                label = self._training_labels[i]
-
-        return label
+        return self._choose_majority(candidates)
