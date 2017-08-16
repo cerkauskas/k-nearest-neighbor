797ebdb9f2bb47ee2615d6974619f92a375042f0
Tomas Čerkauskas <tomas@cerkauskas.lt>
2017-08-16 02:00:20 +0200

    update: predictions with k=[1; 149]

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from KNearestNeighbor import KNearestNeighbor
 from time import time
+import csv
 import numpy as np
 
 def unpickle(file):
@@ -20,41 +21,46 @@ for i in range(1, 6):
     if data is None:
         data = dict['data']
         labels = dict['labels']
-    else:
-        data = np.concatenate((data, dict['data']))
-        labels = np.concatenate((labels, dict['labels']))
+    # else:
+    #     data = np.concatenate((data, dict['data']))
+    #     labels = np.concatenate((labels, dict['labels']))
 
-classifier = KNearestNeighbor(1)
-
-classifier.train(data, labels)
-
-success = 0
-failure = 0
-times = []
 test_data_dict = unpickle('cifar-10-batches-py/test_batch')
 test_data = test_data_dict['data']
 test_labels = test_data_dict['labels']
 
-try:
-    for i in range(len(test_data)):
-        if i % 100 == 0:
-            print "%d predictions have been made" % i
+for k in range(1, 150):
+    classifier = KNearestNeighbor(k)
 
-        features = test_data[i]
-        label = test_labels[i]
-        start = time()
-        prediction = classifier.predict(features)
-        end = time()
+    classifier.train(data, labels)
 
-        times.append(end-start)
+    try:
+        success = 0
+        failure = 0
+        times = []
 
-        if prediction != label:
-            failure += 1
-        else:
-            success += 1
-finally:
-    rate = 100*success / float(success + failure)
-    avg_time = reduce(lambda a, b: a+b, times) / len(times)
+        for i in range(len(test_data)):
+            if i % 20000 == 0:
+                print "%d predictions have been made" % i
 
-    print "Success rate is %f%%" % rate
-    print "Average time to make a prediction is %f" % avg_time
+            features = test_data[i]
+            label = test_labels[i]
+            start = time()
+            prediction = classifier.predict(features)
+            end = time()
+
+            times.append(end-start)
+
+            if prediction != label:
+                failure += 1
+            else:
+                success += 1
+    finally:
+        rate = 100*success / float(success + failure)
+        avg_time = reduce(lambda a, b: a+b, times) / len(times)
+        with open('results.csv', 'a') as result_file:
+            writer = csv.writer(result_file).writerow([
+                k, success, failure, rate, avg_time
+            ])
+
+    print "k=%d is calculated" % k
